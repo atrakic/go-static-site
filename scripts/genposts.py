1917a922e57a70ee581e9c0704175f852360ca96
Admir Trakic <atrakic@users.noreply.github.com>
2026-05-13 20:39:00 +0200

    chore: use git submodule

---
+++ scripts/genposts.py
@@ -0,0 +1,25 @@
+# /// script
+# dependencies = [
+#    "faker",
+#    "mdgen",
+#    "markdown",
+# ]
+# ///
+
+
+""" Generate markdown contents"""
+from faker import Faker
+from mdgen import MarkdownPostProvider
+
+
+def build_post_with_title(fake):
+	"""Build a markdown post prefixed with a makesite title header."""
+	title = fake.sentence(nb_words=5).rstrip('.')
+	post = fake.post(size="medium")
+	return f"<!-- title: {title} -->\n\n{post}"
+
+
+fake = Faker()
+fake.add_provider(MarkdownPostProvider)
+fake_post = build_post_with_title(fake)
+print(fake_post)
